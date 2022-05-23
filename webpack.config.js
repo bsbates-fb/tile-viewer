832c325b5c0d63222194fb71069101d3dc936a38
Ben Clark <bennyc@gmail.com>
2022-05-23 13:32:39 -0400

    Enable automatic CICD (#2)
    * Add simple build directive to support building in PRs.
    
    * Add deployment action.
    
    * Add this branch's name to the triggering branches, just for temporary testing.
    
    * Change copy source dir to be simply 'dist'.
    
    * Add python deployment script (WIP) to massage paths in the delivered bundle to work in our deployed directory structure.
    
    * Fix path to pick up correct modified index.html and ephemeral build directories.
    
    * Fix issue that was causing deploy script to not display the final URL destination correctly.
    
    * Add cloudfront link to deploy results action.
    
    * Add DIST_BASE_URL directive to yarn build invocation.
    
    * Adjust webpack invocation to set CI and DIST_BASE_URL vars correctly.
    
    * Reformat webpack config to accept 'env' input.
    
    * Forgot to add 'install' directive to the build action.
    
    * Fix issues from PR review.
    
    * Change 'master' to 'main'.

--- webpack.config.js
+++ webpack.config.js
@@ -6,63 +6,69 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  context: __dirname,
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist'),
-    // Needed to compile multiline strings in Cesium
-    sourcePrefix: ''
-  },
-  amd: {
-    // Enable webpack-friendly use of require in Cesium
-    toUrlUndefined: true
-  },
-  resolve: {
-    alias: {
-      cesium: path.resolve(__dirname, cesiumSource)
+module.exports = (env) => {
+  console.log("CI: ", env.CI); // 'true'
+  console.log("DIST_BASE_URL: ", env.DIST_BASE_URL); // '<git-sha>-16.x-dist'
+  return {
+    context: __dirname,
+    entry: {
+      app: "./src/index.js",
     },
-    mainFiles: ['module', 'main', 'Cesium']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
+    output: {
+      filename: "app.js",
+      path: path.resolve(__dirname, "dist"),
+      // Needed to compile multiline strings in Cesium
+      sourcePrefix: "",
+    },
+    amd: {
+      // Enable webpack-friendly use of require in Cesium
+      toUrlUndefined: true,
+    },
+    resolve: {
+      alias: {
+        cesium: path.resolve(__dirname, cesiumSource),
       },
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-        test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-        use: [ 'url-loader' ]
-      }
-    ]
-  },
-  plugins: [
-    // Allow pulling envionment variables from .env file
-    new DotenvWebpackPlugin(),
-    // Copy over index.html template into dist directory with the proper includes
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    }),
-    // Copy Cesium Assets, Widgets, and Workers to a static directory
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
-        { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
-        { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }
-      ]
-    }),
-    new webpack.DefinePlugin({
-      // Define relative base path in cesium for loading assets
-      CESIUM_BASE_URL: JSON.stringify('')
-    })
-  ],
-  mode: 'development'
-};
+      mainFiles: ["module", "main", "Cesium"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+          use: ["url-loader"],
+        },
+      ],
+    },
+    plugins: [
+      // Allow pulling envionment variables from .env file
+      new DotenvWebpackPlugin(),
+      // Copy over index.html template into dist directory with the proper includes
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+      }),
+      // Copy Cesium Assets, Widgets, and Workers to a static directory
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: path.join(cesiumSource, cesiumWorkers), to: "Workers" },
+          { from: path.join(cesiumSource, "Assets"), to: "Assets" },
+          { from: path.join(cesiumSource, "Widgets"), to: "Widgets" },
+        ],
+      }),
+      new webpack.DefinePlugin({
+        // Define relative base path in cesium for loading assets
+        CESIUM_BASE_URL: JSON.stringify(
+          env.CI ? `/tileviewer/${env.DIST_BASE_URL}/` : ""
+        ),
+      }),
+    ],
+    mode: "development",
+  };
+}
